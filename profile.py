a35933e4983bcbaec7f6d43fa0287be41ca47473
Fraida Fund <ffund@nyu.edu>
2023-09-05 11:50:28 -0400

    Make Firefox compatible with VNC in a VM

--- profile.py
+++ profile.py
@@ -20,7 +20,7 @@ request = pc.makeRequestRSpec()
 # Set up first host - romeo
 node_romeo = request.XenVM('romeo')
 node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-node_romeo.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt update; /usr/bin/sudo /usr/bin/apt -y install firefox"))
+node_romeo.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
 node_romeo.exclusive = False
 node_romeo.routable_control_ip = True # required for VNC
 node_romeo.startVNC()
