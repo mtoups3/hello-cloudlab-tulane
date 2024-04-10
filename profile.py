89624a542f947599f1ac83c802016f31e0f0492d
M. Toups <mtoups3@tulane.edu>
2024-04-10 15:30:12 -0500

    fix profile, remove VNC

--- profile.py
+++ profile.py
@@ -22,12 +22,14 @@ node_greenwave_cloudlab.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-o
 node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
 # commands below replace MOTD with Tulane specific stuff --mt
 node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command='/usr/bin/sudo /bin/rm /etc/update-motd.d/*')) # remove default Ubuntu stuff
-node_greenwave_cloudlab.addService(rspec.Execute('/bin/sh','wget -O /etc/profile.d/00tulane-profile.sh https://raw.githubusercontent.com/mtoups3/hello-cloudlab-tulane/main/00tulane-profile.sh'))
+node_greenwave_cloudlab.addService(rspec.Execute(shell="bash",command='/usr/bin/sudo /usr/bin/wget -O /etc/profile.d/00tulane-profile.sh https://raw.githubusercontent.com/mtoups3/hello-cloudlab-tulane/main/00tulane-profile.sh'))
 node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command='/usr/bin/sudo /usr/bin/chmod +x /etc/profile.d/00tulane-profile.sh'))
 
-node_greenwave_cloudlab.exclusive = False
-node_greenwave_cloudlab.routable_control_ip = True # required for VNC
-node_greenwave_cloudlab.startVNC()
+node_greenwave_cloudlab.exclusive = False # use False for shared Xen VMs
+
+### disabling VNC to try to make this easier to start up on Cloudlab
+#node_greenwave_cloudlab.routable_control_ip = True # required for VNC
+#node_greenwave_cloudlab.startVNC()
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
