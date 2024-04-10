317a39f5f85d1be90677b6005f8d52ac8394055c
M. Toups <mtoups3@tulane.edu>
2024-04-10 17:08:43 -0500

    profile clean up to start faster

--- profile.py
+++ profile.py
@@ -19,11 +19,15 @@ request = pc.makeRequestRSpec()
 # Set up first host - greenwave-cloudlab (foremerly romeo)
 node_greenwave_cloudlab = request.XenVM('greenwave-cloudlab')
 node_greenwave_cloudlab.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
+
+# comment out below - we don't need firefox anymore
+#node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
 # commands below replace MOTD with Tulane specific stuff --mt
 node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command='/usr/bin/sudo /bin/rm /etc/update-motd.d/*')) # remove default Ubuntu stuff
 node_greenwave_cloudlab.addService(rspec.Execute(shell="bash",command='/usr/bin/sudo /usr/bin/wget -O /etc/profile.d/00tulane-profile.sh https://raw.githubusercontent.com/mtoups3/hello-cloudlab-tulane/main/00tulane-profile.sh'))
 node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command='/usr/bin/sudo /usr/bin/chmod +x /etc/profile.d/00tulane-profile.sh'))
+# get rid of annoying ubuntu legal stuff
+node_greenwave_cloudlab.addService(rspec.Execute(shell="bash",command='/usr/bin/sudo rm /etc/legal'))
 
 node_greenwave_cloudlab.exclusive = False # use False for shared Xen VMs
 
