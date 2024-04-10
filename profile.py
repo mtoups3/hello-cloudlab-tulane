480f36acf58b3a149255172b475eb1778604a77d
M. Toups <mtoups3@tulane.edu>
2024-04-09 20:01:00 -0500

    test updating profile.py with new hostname

--- profile.py
+++ profile.py
@@ -1,6 +1,6 @@
 """This topology has one host. Also, the `firefox` web browser is installed.
 
-To use this topology, follow the instructions at: [Hello CloudLab](https://teaching-on-testbeds.github.io/hello-cloudlab/)
+Spring 2024: modified by M. Toups to change hostname (more changes to come)
 
 """
 
@@ -18,12 +18,12 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Set up first host - romeo
-node_romeo = request.XenVM('romeo')
-node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
-node_romeo.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
-node_romeo.exclusive = False
-node_romeo.routable_control_ip = True # required for VNC
-node_romeo.startVNC()
+node_greenwave_cloudlab = request.XenVM('greenwave-cloudlab')
+node_greenwave_cloudlab.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
+node_greenwave_cloudlab.addService(rspec.Execute(shell="bash", command="/usr/bin/sudo /usr/bin/apt purge firefox; /usr/bin/sudo /usr/bin/snap remove firefox; /usr/bin/sudo /usr/bin/add-apt-repository ppa:mozillateam/ppa -y ; /usr/bin/sudo /usr/bin/apt -y install firefox-esr; /usr/bin/sudo /usr/bin/ln -s /usr/bin/firefox-esr /usr/local/bin/firefox"))
+node_greenwave_cloudlab.exclusive = False
+node_greenwave_cloudlab.routable_control_ip = True # required for VNC
+node_greenwave_cloudlab.startVNC()
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
